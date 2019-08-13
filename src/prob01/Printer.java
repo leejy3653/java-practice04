2fdacf6333dcec6757c4e0f5dacc43ca3cc29e16
bit <bit@DESKTOP-1LOO2I6>
2019-08-13 14:11:45 +0900

    2019.08.13		14:11 강사님

--- src/prob01/Printer.java
+++ src/prob01/Printer.java
@@ -17,8 +17,38 @@ public class Printer{
 	public void println(String string) {
 		System.out.println(string);
 	}
-
+	
 	public void println(int i, boolean b, double c, String string) {
 		System.out.println(""+ i + " " + b + " " + c + " " + string);
 	}	
+	
+	//public <T, P, Q> void println(T t) {
+	//	System.out.println(t);
+	//}
+	//public int sum(int a, int b, int c) {
+	//	return 0;
+	//}
+	//public int sum(int[] a) {
+	//	return 0;
+	//}
+	
+	//가변변수
+	public int sum(int...nums) {
+		int sum = 0;
+		for(int i : nums) {
+			sum += i;
+		}
+		return sum;
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public <T> void println(T... ts) {
+		for(T t : ts) {
+			System.out.println(t);
+		}
+		
+	}
+
+	
 }
--- src/prob01/PrinterTest.java
+++ src/prob01/PrinterTest.java
@@ -5,7 +5,8 @@ public class PrinterTest {
 	public static void main(String[] args) {
 		Printer printer = new Printer();
 
-		printer.println( 10 );
+		printer.println( 10 )
+		;
 		printer.println( true );
 		printer.println( 5.7 );
 		printer.println( "홍길동" );
